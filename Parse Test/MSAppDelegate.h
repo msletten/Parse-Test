995d53fc2f8c62fe8ba632ff3d35a679124177bf
Mat Sletten <matsletten@lyellenimac.bbbb.net>
2014-05-30 17:17:35 -0700

    Initial Commit

---
+++ Parse Test/MSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MSAppDelegate.h
+//  Parse Test
+//
+//  Created by Mat Sletten on 5/30/14.
+//  Copyright (c) 2014 Mat Sletten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Parse Test/MSViewController.h
@@ -0,0 +1,13 @@
+//
+//  MSViewController.h
+//  Parse Test
+//
+//  Created by Mat Sletten on 5/30/14.
+//  Copyright (c) 2014 Mat Sletten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSViewController : UIViewController
+
+@end
